9ffb0bf7bbcc24afb570574e9f9c80d2f35fd0c3
Rafal Lazarek <rflazarek@gmail.com>
2022-02-14 23:13:27 +0100

    homework week V

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const firstName='Rafał';
 const age=28;
 
-alert('SIEMA!');
+/**alert('SIEMA!');**/
 console.log(`siema! Nazywam się ${firstName} i mam ${age} lat`);
 
+alert(`Siema! Nazywam się ${firstName} i mam ${age} lat`);
+
 const heading = document.querySelector('.header-h1--js');
-heading.innerHTML= 'testujemy';
+heading.innerHTML= 'test wpisu przes JS';
