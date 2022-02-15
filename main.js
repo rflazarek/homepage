8de8afa16eba62b797dbef3f47e86353f5231c79
Rafal Lazarek <rflazarek@gmail.com>
2022-02-15 16:17:32 +0100

    corrected homework V

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const firstName = "Rafał";
 const age = 28;
 
-/**alert('SIEMA!');**/
+/**alert("SIEMA!");**/
 console.log(`siema! Nazywam się ${firstName} i mam ${age} lat`);
 
 alert(`Siema! Nazywam się ${firstName} i mam ${age} lat`);
 
 const heading = document.querySelector(".header-h1--js");
 heading.innerHTML = "test wpisu przes JS";
+
+const hello = (firstName, age) => {
+  console.log(
+    `Witaj drogi odwiedzający! Nazywam się ${firstName} i mam ${age} lat`
+  );
+};
+
+hello("Rafał", 28);
